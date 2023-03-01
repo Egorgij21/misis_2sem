d4bb784c4a3e7e2fa3b6f407d5450fdb56ad3382
Egorgij21 <70803676+Egorgij21@users.noreply.github.com>
2023-03-01 11:11:30 +0300

    rational

---
+++ aparin_g_m/rational/rational/rational.h
@@ -0,0 +1,63 @@
+#include<iosfwd>
+
+
+
+class Rational{
+
+int32_t num = 0;
+int32_t denum = 1;
+
+public:
+    Rational();
+    Rational(const int32_t);
+    Rational(const int32_t num, const int32_t denum);
+    ~Rational() = default;
+
+
+
+    std::ostream& writeTo(std::ostream&) const;
+	std::istream& readFrom(std::istream&);
+
+
+
+    Rational& operator=(const Rational&);
+    Rational& operator=(const int32_t&);
+
+
+
+    Rational operator+(const Rational&) const;
+    Rational operator-(const Rational&) const;
+    Rational operator-() const;
+    Rational operator*(const Rational&) const;
+    Rational operator/(const Rational&) const;
+    Rational operator++();
+    Rational operator++(int);
+
+	Rational operator--();
+    Rational operator--(int);
+
+
+
+
+    Rational& operator+=(const Rational&);
+    Rational& operator-=(const Rational&);
+    Rational& operator*=(const Rational&);
+    Rational& operator/=(const Rational&);
+
+
+
+    bool operator<(const Rational&) const;
+    bool operator>(const Rational&) const;
+    bool operator<=(const Rational&) const;
+    bool operator>=(const Rational&) const;
+    bool operator==(const Rational&) const;
+    bool operator!=(const Rational&) const;
+
+
+    double toDouble() const;
+    Rational& sqrt() const;
+private:
+    void reduce();
+    int32_t gcd(int32_t num, int32_t denum) const;
+    Rational normalize(Rational&) const;
+};
